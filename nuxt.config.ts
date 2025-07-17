13bb1eb40c9b54234c18ef4fbbe2522d9e00a18a
ytnd0928 <yetianxiongfei@gmail.com>
2025-07-18 01:36:08 +0900

    ビルドコマンドを「generate」から「build」に変更し、アーティファクトの出力先を「.amplify-hosting/static」から「.output/public」に更新。Nuxtの互換性日付を「2025-07-16」から「2024-04-03」に変更し、Nitroのプリセットを「aws-amplify」から「node」に変更。package.jsonとpackage-lock.jsonでNuxtのバージョンを「3.17.7」から「3.16.0」にダウングレードし、Nodeとnpmのエンジン要件を追加。

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,6 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: "2025-07-16",
+  compatibilityDate: "2024-04-03",
   devtools: { enabled: true },
   modules: ["@vueuse/nuxt"],
   css: [
@@ -16,13 +16,7 @@ export default defineNuxtConfig({
     transpile: ["vuetify"],
   },
   nitro: {
-    preset: "aws-amplify",
-    experimental: {
-      wasm: true,
-    },
-  },
-  experimental: {
-    payloadExtraction: false,
+    preset: "node",
   },
   app: {
     head: {
