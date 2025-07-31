80af150ed2094941d91a47738ab7fee2abc2f0d0
ytnd0928 <yetianxiongfei@gmail.com>
2025-08-01 00:27:08 +0900

    [add]add

--- app.vue
+++ app.vue
@@ -1,3 +1,11 @@
 <template>
   <NuxtPage />
 </template>
+<script setup>
+import { onMounted } from "vue";
+import clarity from "@microsoft/clarity";
+
+onMounted(() => {
+  clarity.init("YOUR_CLARITY_PROJECT_ID");
+});
+</script>
